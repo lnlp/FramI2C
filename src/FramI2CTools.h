77e3d0f1e7ddd3d2aa4b14c3b8609f679c147ecf
Leonel Lopes Parente <lnlp@appfactors.com>
2019-09-17 17:26:59 +0200

    Replaced all printf() and sprintf() calls with print() and println() for better compatibility. Added printHex(), printChars() and printSpaces().

--- src/FramI2CTools.h
+++ src/FramI2CTools.h
@@ -2,9 +2,6 @@
  *
  * Description:  Utilities / helper functions for FramI2C.
  * 
- * Note:         Initially designed for ESP8266 therefore uses printf.
- *               To be replaced with standard Arduino print and println in a future version.
- * 
  * Author:       Leonel Lopes Parente
  * License:      MIT (see LICENSE file in repository root)
  * 
@@ -17,29 +14,147 @@
 #include "FramI2C.h"
 
 
+void printChars(Stream& stream, char ch, uint8_t count, bool linefeed = false)
+{
+    for (uint8_t i = 0; i < count; ++i)
+    {
+        stream.print(ch);
+    }
+    if (linefeed)
+    {
+        stream.println();
+    }
+}
+
+
+void printSpaces(Stream& stream, uint8_t count, bool linefeed = false)
+{
+    printChars(stream, ' ', count, linefeed);
+}
+
+
+void printHex(Stream& stream, uint32_t value, bool prefix = true, uint8_t width = 0, bool uppercase = true)
+{
+    // Prints hex value to stream.
+    // Value is prefixed with "0x" unless prefix is false.
+    // If string length is uneven then a leading zero is added (0xF => "0x0F").
+    // If width > value string length then multiple leading zeros will be added.
+    // Note that width does not include the "0x" prefix.
+
+    uint8_t zeros;
+    String s(value, HEX);
+    uint8_t len = s.length();
+
+    if (uppercase)
+    {
+        s.toUpperCase();
+    }
+
+    if (width == 0)
+    {
+        zeros = len % 2;
+    }
+    else
+    {
+        zeros = width <= len ? 0 : width - len;
+    }
+
+    if (prefix)
+    {
+        stream.print("0x");
+    }
+
+    if (zeros > 0)
+    {
+        printChars(stream, '0', zeros);
+    }
+    stream.print(s);
+}
+
+
 void printFramInfo(Stream& stream, FramI2C& fram, const char* const instanceName = "FramI2C")
 {
     if (!fram.isInitialized())
     {
-        stream.printf("FramI2C is not initialized.\n\n");
+        stream.print(instanceName);
+        stream.println(F(" is not initialized.\n"));
     }
     else
     {
-        stream.printf("%s properties:\n\n", instanceName);
-        stream.printf("  Density:           %u kilobits\n", fram.density());    
-        stream.printf("  I2C address:       0x%X\n", fram.i2cAddress());
-        stream.printf("  Memory size:       0x%X bytes\n", fram.memorySize());
-        stream.printf("  Page size:         0x%X bytes\n", fram.pageSize());
-        stream.printf("  Number of pages:   %u\n", fram.pageCount());
-        stream.printf("  Type buffer size:  %u bytes\n\n", fram.typebufferSize());
+        // stream.printf("%s properties:\n\n", instanceName);        
+        stream.print(instanceName);
+        stream.println(F(" properties:"));
+        printChars(stream, '-', strlen(instanceName) + 12);
+        stream.println();
+
+        // stream.printf("Density:          %u kb\n", fram.density()); 
+        stream.print(F("Density:"));  
+        printSpaces(stream, 10);  
+        stream.print(fram.density(), DEC);    
+        stream.println(F(" kb"));
+
+        // stream.printf("I2C address:      0x%X\n", fram.i2cAddress());
+        stream.print(F("I2C address:"));
+        printSpaces(stream, 6);
+        printHex(stream, fram.i2cAddress());
+        stream.println();
+
+        // stream.printf("Memory size:      0x%X B\n", fram.memorySize());
+        stream.print(F("Memory size:"));
+        printSpaces(stream, 6);
+        if (fram.memorySize() < 1024)
+        {
+            stream.print(fram.memorySize());
+            stream.println(F(" B"));
+        }
+        else
+        {
+            stream.print(fram.memorySize() / 1024);
+            stream.println(F(" kB"));
+        } 
+
+        // stream.printf("Page size:        0x%X B\n", fram.pageSize());
+        stream.print(F("Page size:"));
+        printSpaces(stream, 8);        
+        if (fram.pageSize() < 1024)
+        {
+            stream.print(fram.pageSize());
+            stream.println(F(" B"));
+        }
+        else
+        {
+            stream.print(fram.pageSize() / 1024);
+            stream.println(F(" kB"));
+        }    
+
+        // stream.printf("Page count:       %u\n", fram.pageCount());
+        stream.print(F("Page count:"));
+        printSpaces(stream, 7);        
+        stream.println(fram.pageCount(), DEC);
+
+        // stream.printf("Type buffer size: %u B\n\n", fram.typebufferSize());
+        stream.print(F("Type buffer size: "));
+        stream.print(fram.typebufferSize());
+        stream.println(F(" B"));           
     }
 
 	if (fram.isDeviceIdSupported())	
 	{
-    	stream.println("  Device ID is supported");		
-    	stream.printf("  Manufacturer ID:   %u\n", fram.manufacturerId());		
-    	stream.printf("  Product ID:        0x%04X\n\n", fram.productId());		
+        stream.print(F("Device ID:"));
+        printSpaces(stream, 8);        		
+        stream.println(F("supported"));		
+
+        // stream.printf("Manufacturer ID:  %u\n", fram.manufacturerId());		
+        stream.print(F("Manufacturer ID:  "));
+        stream.println(fram.manufacturerId(), DEC);
+
+        // stream.printf("Product ID:       0x%04X\n\n", fram.productId());	
+        stream.print(F("Product ID:"));
+        printSpaces(stream, 7);
+        printHex(stream, fram.productId(), true, 3);
+        stream.println();
 	} 
+    stream.println();
 }
 
 
@@ -57,61 +172,61 @@ void printResultCodeDescription(
     
     if (!(resultcode == FramI2C::ResultCode::Success || resultcode == FramI2C::ResultCode::Uninitialized))
     {
-        stream.print("Error: ");
+        stream.print(F("Error: "));
     }
 
     switch (resultcode)
     {
         case FramI2C::ResultCode::Success:		
-            stream.print("Success.");
+            stream.print(F("Success."));
             break;
         case FramI2C::ResultCode::I2CBufferOverflowError:		
-            stream.print("I2C buffer overflow.");
+            stream.print(F("I2C buffer overflow."));
             break;
         case FramI2C::ResultCode::I2CAddressNackError:		
-            stream.print("I2C address not acknowleged (nack).");
+            stream.print(F("I2C address not acknowleged (nack)."));
             break;
         case FramI2C::ResultCode::I2CDataNackError:		
-            stream.print("I2C data not acknowleged (nack).");
+            stream.print(F("I2C data not acknowleged (nack)."));
             break;
         case FramI2C::ResultCode::I2CLineBusyError:		
-            stream.print("I2C line is busy.");
+            stream.print(F("I2C line is busy."));
             break;
         case FramI2C::ResultCode::I2CReadError:
-            stream.print("I2C Read Error.");
+            stream.print(F("I2C Read Error."));
             break;
         case FramI2C::ResultCode::I2CWriteError:
-            stream.print("I2C Write Error.");
+            stream.print(F("I2C Write Error."));
             break;
         case FramI2C::ResultCode::I2CUnknownTwiResultCode:
-            stream.print("I2C Unknown TwiResultCode.");
+            stream.print(F("I2C Unknown TwiResultCode."));
             break;
         case FramI2C::ResultCode::NullPtrError:		
-            stream.print("Null pointer.");
+            stream.print(F("Null pointer."));
             break;
         case FramI2C::ResultCode::NotInitializedError:		
-            stream.print("FramI2C not initialized.");
+            stream.print(F("FramI2C not initialized."));
             break;
         case FramI2C::ResultCode::AllreadyInitializedError:		
-            stream.print("FramI2C already initialized (differently).");
+            stream.print(F("FramI2C already initialized (differently)."));
             break;
         case FramI2C::ResultCode::UnsupportedDensityError:		
-            stream.print("Unsupported density.");
+            stream.print(F("Unsupported density."));
             break;
         case FramI2C::ResultCode::InvalidPageError:		
-            stream.print("Invalid page.");
+            stream.print(F("Invalid page."));
             break;			
         case FramI2C::ResultCode::PageSizeRangeError:		
-            stream.print("Out of page size range.");
+            stream.print(F("Out of page size range."));
             break;
         case FramI2C::ResultCode::BufferAllocationFailedError:		
-            stream.print("Type buffer allocation failed.");
+            stream.print(F("Type buffer allocation failed."));
             break;
         case FramI2C::ResultCode::BufferOverflowError:		
-            stream.print("Type too large for buffer.");
+            stream.print(F("Type too large for buffer."));
             break;
         case FramI2C::ResultCode::Uninitialized:		
-            stream.print("Uninitialized.");
+            stream.print(F("Uninitialized."));
             break;
     }
     if (postfixSpace)
@@ -132,31 +247,34 @@ FramI2C::ResultCode hexdumpFram(
     const uint16_t address, 
     const uint32_t byteCount, 
     const bool header = true, 
-    const char* const message = nullptr, 
+    const char* const message = "", 
     const uint8_t linefeeds = 1)
 {
-    char pageString[9] = "";
+    // char pageString[9] = "";
+    String pageStr;
 
     if (header)
     {
+        // Print header
         if (fram.pageCount() > 1)
         {
             // Show page in header only if FRAM has multiple pages.
-            sprintf(pageString, "page %u, ", page);
+            pageStr = "page " + String(page) + ", ";
         }
-        if (message == nullptr || strlen(message) == 0)
-        {
-            stream.printf("FRAM hexdump - %saddress 0x%X, 0x%X (%u) bytes\n", pageString, address, byteCount, byteCount);
-        }
-        else
-        {
-            stream.printf("FRAM hexdump %s - %saddress 0x%X, 0x%X (%u) bytes\n", message, pageString, address, byteCount, byteCount);
-        }
+        stream.print(F("FRAM hexdump - "));
+        stream.print(pageStr);
+        stream.print(F("address "));
+        printHex(stream, address);
+        stream.print(F(", "));
+        printHex(stream, byteCount);
+        stream.print(F(" ("));
+        stream.print(byteCount, DEC);
+        stream.println(F(") bytes"));
     }
 
     if (byteCount == 0)
     {
-        stream.println("Byte count is 0 (framHexdump).");
+        stream.println(F("Byte count is 0 (framHexdump)."));
         for (uint8_t i = 0; i < linefeeds; ++i)
         {
             stream.println();
@@ -178,26 +296,27 @@ FramI2C::ResultCode hexdumpFram(
 
     if (header)
     {
+        // Print label row
         stream.println();
-
-        // Output header
-        stream.print("     ");
+        printSpaces(stream, 5);
         for (uint8_t i = 0; i < 16; ++i)
         {
             if (i == 8)
             {
-                stream.print("  ");
+                printSpaces(stream, 2);
             }
-            stream.printf(" %02X", i);
+            stream.print(' ');
+            printHex(stream, i, false);
         }
-        stream.print("\n     ");
+        stream.println();
+        printSpaces(stream, 5);
         for (uint8_t i = 0; i < 16; ++i)
         {
             if (i == 8)
             {
-                stream.print("  ");
+                printSpaces(stream, 2);
             }
-            stream.printf(" --");
+            stream.print(" --");
         }
         stream.println();
     }
@@ -210,14 +329,15 @@ FramI2C::ResultCode hexdumpFram(
     {
         ++lineNr;
         // Output first adreslabel and indent
-        stream.printf("%04X:", address & 0xFFF0);
+        printHex(stream, address & 0xFFF0, false, 4); 
+        stream.print(":");
         for (uint8_t i = 0; i < firstByteColumnOffset; ++i)		
         {
             if (i == 8 && (firstByteColumnOffset) >= 8)
             {
-                stream.print("  ");
+                printSpaces(stream, 2);
             } 
-            stream.print("   ");
+            printSpaces(stream, 3);
         }
     }
 
@@ -239,7 +359,8 @@ FramI2C::ResultCode hexdumpFram(
                 stream.println();
             }
             // Write address label
-            stream.printf("%04X:", addr & 0xFFF0);
+            printHex(stream, addr & 0xFFF0, false, 4);             
+            stream.print(":");
         }
         if ((addr & 0xF) ==  8)
         {
@@ -253,7 +374,8 @@ FramI2C::ResultCode hexdumpFram(
             printResultCodeDescription(stream, resultcode, 0);
             break;
         }
-        stream.printf(" %02X", value);
+        stream.print(" ");        
+        printHex(stream, value, false, 2);             
         ++addr;
     }
     if (resultcode == FramI2C::ResultCode::Success)
